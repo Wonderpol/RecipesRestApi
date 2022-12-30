06efb6295fd5e0c429b3b8eab867d26e522b1dd8
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-12-30 21:16:51 +0100

    refactor: added http status 403 if user with email already exists

--- src/main/java/com/example/recipesapi/v1/security/exception/UserAlreadyExistsException.java
+++ src/main/java/com/example/recipesapi/v1/security/exception/UserAlreadyExistsException.java
@@ -1,5 +1,10 @@
 package com.example.recipesapi.v1.security.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.FORBIDDEN)
+
 public class UserAlreadyExistsException extends RuntimeException {
     public UserAlreadyExistsException(final String message) {
         super(message);
